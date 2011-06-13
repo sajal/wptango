5f09e3ccabf69e93b0cdbc79188fbba403b587b5
Sajal Kayan <sajal@thaindian.com>
2011-06-13 14:07:58 +0700

    starting to make a management command for feed processing

---
+++ tester/management/commands/process_wpt.py
@@ -0,0 +1,7 @@
+from django.core.management.base import BaseCommand, CommandError
+
+class Command(BaseCommand):
+    args = '<None no need>'
+    help = 'posts new test requests to wpt, fetches results for previous ones. best to run it every min or smthn'
+    def handle(self, *args, **options):
+        print "RUNNING"
