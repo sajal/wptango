e64fda754d02171597db4434a3a405057f56c73d
Sajal Kayan <sajal@thaindian.com>
2011-06-23 02:52:26 +0700

    logged user sees only their items in list and only their urlreport

--- tester/views.py
+++ tester/views.py
@@ -1,30 +1,33 @@
 # Create your views here.
 from wptango.tester.models import *
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseForbidden
 from django.db import connection
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 
 @login_required
 def listurls(request):
-  r = runnable.objects.all()
-  return render_to_response('list.html', {'tests': r, "user": request.user})
+    r = runnable.objects.all()
+    r = r.filter(allowed_users = request.user)
+    return render_to_response('list.html', {'tests': r, "user": request.user})
   
 @login_required  
 def urlreport(request, id):
-  r = runnable.objects.get(id=int(id))
-  tests = testrun.objects.filter(runnable=r).filter(status=4)
-  return render_to_response('url.html', {'tests': tests, 'url': r.name, "user": request.user})
+    r = runnable.objects.get(id=int(id))
+    if request.user not in r.allowed_users.all():
+        return HttpResponseForbidden("Not Allowed!")
+    tests = testrun.objects.filter(runnable=r).filter(status=4)
+    return render_to_response('url.html', {'tests': tests, 'url': r.name, "user": request.user})
 
 
 def processurls(request):
-  for r in runnable.objects.all():
-    r.check_schedule()
-  for test in testrun.objects.filter(status=1):
-    test.submit_to_wpt()
-  for test in testrun.objects.filter(status=2):
-    test.get_wpt_results()    
-  return HttpResponse("OK")
+    for r in runnable.objects.all():
+        r.check_schedule()
+    for test in testrun.objects.filter(status=1):
+        test.submit_to_wpt()
+    for test in testrun.objects.filter(status=2):
+        test.get_wpt_results()    
+    return HttpResponse("OK")
   
 """
 def addurl(request):
